b5c39df6ea6c222429c485b59c749a4ed2f5afa5
tagasouichiro <stitow14@gmail.com>
2018-02-11 23:04:44 +0900

    Add slice assert func

--- sort/quick_test.go
+++ sort/quick_test.go
@@ -6,8 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var s []int = []int{3, 5, 6, 1, 3, 4, 6}
+
 func TestQuickSort(t *testing.T) {
-	s := []int{3, 5, 6, 1, 3, 4, 6}
 	res := quickSort(s)
 	assert.Equal(t, s, []int{})
+}
+
+func assertSlice(t *testing.T, e, a []int) {
+	if len(e) != len(a) {
+		t.Errorf(
+			"slice size not matcha:\n expected:%d \n actual:%d \n",
+			len(e),
+			len(a),
+		)
+	}
+	for i, v := range e {
+		if v != a[i] {
+			t.Errorf(
+				"slice value not match(i: %d):\n expected:%v \n actual:%v \n",
+				i,
+				e,
+				a,
+			)
+		}
+	}
 }
